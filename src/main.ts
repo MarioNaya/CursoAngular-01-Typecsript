3374eb676c1f4d4383caaa5d4f94917ede50d059
mario <marionayadeluis@gmail.com>
2025-07-23 01:32:18 +0200

    Inicialización del proyecto

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+//import './topics/01-basic-types.ts'
+import './topics/02-object-interface.ts'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'Hola Mundo';
+
+console.log('Hola Mundo!!!');
